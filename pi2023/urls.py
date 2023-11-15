fcff556a82e860360256649d492c07435bc20c21
camilamuemura <134243688+camilamuemura@users.noreply.github.com>
2023-11-15 11:47:40 -0300

    Update urls.py

--- pi2023/urls.py
+++ pi2023/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from . import views
+from .views import dashboard
+from .views import dados_grafico
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    #path('', views.index_exemplo, name='index-exemplo'),
-    path('', views.index, name='index'),
-    path('sobre_o_projeto.html', views.sobre_o_projeto, name='sobre_o_projeto'),
-    path('universidade.html', views.universidade, name='universidade'), 
-    path('index.html', views.home, name='home')
+    # path('', views.index_exemplo, name='index-exemplo'),
+    path("", views.index, name="index"),
+    path("sobre_o_projeto.html", views.sobre_o_projeto, name="sobre_o_projeto"),
+    path("universidade.html", views.universidade, name="universidade"),
+    path("index.html", views.home, name="home"),
+    path("dashboard.html", dashboard, name="dashboard"),
+    path("dados_grafico/", dados_grafico, name="dados_grafico"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
