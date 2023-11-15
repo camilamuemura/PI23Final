99a2d97a3d624f6d6cb6b14543bd952ff3cfe474
camilamuemura <134243688+camilamuemura@users.noreply.github.com>
2023-11-15 11:47:25 -0300

    Update views.py

--- pi2023/views.py
+++ pi2023/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
+from django.db import models
+from .models import Dados
+from .models import GraficoEstados
+from django.core.serializers import serialize
+from django.http import JsonResponse
+
 
 def index(request):
-    return render(request, 'index.html') 
+    return render(request, "index.html")
+
 
 def sobre_o_projeto(request):
-    return render(request, 'sobre_o_projeto.html') 
+    return render(request, "sobre_o_projeto.html")
+
 
 def universidade(request):
-    return render(request, 'universidade.html') 
+    return render(request, "universidade.html")
+
 
 def home(request):
-    return render(request, 'index.html') 
+    return render(request, "index.html")
 
+
+def dashboard(request):
+    return render(request, "dashboard.html")
+
+
+def dados_grafico(request):
+    dados = GraficoEstados.objects.values("Estado", "Ocorrencias")
+    return JsonResponse(list(dados), safe=False)
